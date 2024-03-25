63da9b01ba0373e6ec367a1010cfe20c47001981
Kutim <1252900197@qq.com>
2024-03-25 14:27:05 +0800

    添加 sovity  策略 测试

--- extensions/policy/policy-always-true/build.gradle.kts
+++ extensions/policy/policy-always-true/build.gradle.kts
@@ -9,4 +9,10 @@ dependencies {
     api(project(":spi:control-plane:control-plane-spi"))
     implementation(project(":extensions:common:api:api-core"))
 
+    testImplementation(project(":core:common:junit"))
+    testImplementation(project(":core:control-plane:control-plane-core"))
+    testImplementation(project(":core:data-plane-selector:data-plane-selector-core"))
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
 }
--- extensions/policy/policy-referring-connector/build.gradle.kts
+++ extensions/policy/policy-referring-connector/build.gradle.kts
@@ -8,4 +8,9 @@ dependencies {
     api(project(":spi:common:policy-engine-spi"))
     api(project(":spi:control-plane:contract-spi"))
 
+    testImplementation(project(":core:common:junit"))
+
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.params)
 }
